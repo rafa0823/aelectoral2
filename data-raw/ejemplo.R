e970ca61af3c09af4fa74c37916769d218841ac7
EAMI91 <emiliomorones@gmail.com>
2022-04-19 17:13:27 -0500

    excluir casillas especiales (historia #AEEB-1)

--- data-raw/ejemplo.R
+++ data-raw/ejemplo.R
@@ -4,5 +4,10 @@ ejemplo <- Electoral$new(inicial = "~/Dropbox (Selva)/Ciencia de datos/Consultor
   ano = 2011, tipo = "Local", eleccion = "Municipio", entidad = "michoacan", nivel = "casilla")
 
 
+ejemplo2 <- Electoral$new(inicial = "~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/Resultados definitivos",
+                         ano = 2012, tipo = "Federal", eleccion = "Presidencia", nivel = "casilla")
+
+
+ejemplo2$eliminar_especiales()
 
 

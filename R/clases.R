8c8a3d0fc2d0e0f2f192ca8e8802b87e15dd6eb1
EAMI91 <emiliomorones@gmail.com>
2022-04-21 14:05:37 -0500

    Hacer joins de caso federal. Historia #AEEB-6

--- R/clases.R
+++ R/clases.R
@@ -34,14 +34,39 @@ Electoral <- R6::R6Class("Electoral",
 
                                          self$obtener_bd()
 
+                                         self$join_bd()
+
                                        },
                                        obtener_bd = function(){
                                          self$bd <- leer_base(inicial = self$inicial, ano = self$ano, tipo = self$tipo, eleccion = self$eleccion,
-                                                            entidad = self$entidad, normal = self$normal, nivel = self$nivel) %>%
+                                                              entidad = self$entidad, normal = self$normal, nivel = self$nivel) %>%
                                            limpiar_base()
-
                                          #indica si hay columnas con números pues probablemente sea un error manual
                                          revisar_nombres(self$bd)
+                                       },
+                                       join_bd = function(){
+                                         #caso federal
+                                         if(n_distinct(self$tipo) == 1 & self$tipo == "Federal"){
+                                           if(n_distinct(self$ano) == 1){ # por casilla
+
+                                             self$bd <- self$bd %>% imap(~{
+                                               sufijo(.x, .y,"clave_casilla")
+                                             }) %>%
+                                               reduce(full_join,by = "clave_casilla")
+
+                                           } else{ #por sección
+                                             self$bd <- self$bd %>% imap(~{
+                                               .x %>% group_by(seccion) %>% tidyr::nest() %>%
+                                                 sufijo(.y,"seccion")
+                                                 }) %>%
+                                               reduce(full_join, by = "seccion")
+
+                                           }
+                                         }
+                                         # caso local
+                                         if(n_distinct(self$tipo) == 1 & self$tipo == "Local"){
+
+                                         }
                                        },
                                        eliminar_especiales = function(){
                                          if(self$nivel == "casilla"){
--- R/lectura.R
+++ R/lectura.R
@@ -21,24 +21,30 @@ leer_base <- function(inicial, ano, tipo, eleccion, entidad, normal, nivel){
     wd <- glue::glue("{wd}/{eleccion}/{entidad}_")
     if(normal) wd <- glue::glue("{wd}normal_{nivel}.csv") else wd <- glue::glue("{wd}extraordinaria_{nivel}.csv")
   }
-  return(readr::read_csv(wd))
+
+  res <- wd %>% map(~readr::read_csv(.x)) %>% purrr::set_names(glue::glue("{tipo}/{ano}/{eleccion}"))
+  return(res)
 }
 
 limpiar_base <- function(bd){
   # quitar enters
-  bd <- bd %>% janitor::clean_names() %>%
-    mutate(across(where(is.character),
-                  ~stringr::str_replace_all(string = .x,pattern = "\\n",replacement = " ")))
+  bd <- bd %>% map(~{
+    .x %>% janitor::clean_names() %>%
+      mutate(across(where(is.character),
+                    ~stringr::str_replace_all(string = .x,pattern = "\\n",replacement = " ")))
+  })
 
   return(bd)
 }
 
 revisar_nombres <- function(bd){
-  nombres <- bd %>% select(-contains("independiente")) %>% #en el caso de independientes sí pueden haber números
-    select(matches("[[:digit:]]")) %>% names
-  if(length(nombres)>0) warning(glue::glue("\n Revisar las columnas {paste(nombres, collapse = ', ')} \n\n Las columnas que tienen números es porque los nombres están repetidos y readr les agrega el número de la columna.
+  bd %>% imap(~{
+    nombres <- .x %>% select(-contains("independiente")) %>% #en el caso de independientes sí pueden haber números
+      select(matches("[[:digit:]]")) %>% names
+    if(length(nombres)>0) warning(glue::glue("Base: {.y} \n Revisar las columnas {paste(nombres, collapse = ', ')} \n\n Las columnas que tienen números es porque los nombres están repetidos y readr les agrega el número de la columna.
                                          \n Esto es por un error manual y lo deberá arreglar Andrés."))
-  return(nombres)
+  })
+
 }
 
 eliminar_especiales <- function(bd){
@@ -52,4 +58,11 @@ eliminar_votoExtranjero <- function(bd){
   aux <- bd %>%  filter(seccion != 0)
   message(glue::glue("Se eliminaron {nrow(bd)-nrow(aux)} casillas"))
   return(aux)
+}
+
+sufijo <- function(bd, nombre, nivel){
+  aux <- nombre %>% str_split("/") %>% pluck(1)
+  s <- glue::glue("{substr(aux[3],1,1)}{substr(aux[1],1,1)}_{substr(aux[2],3,4)}") %>% tolower
+  bd <- bd %>% rename_at(vars(-!!rlang::sym(nivel)),function(x) paste(x, s, sep = "_"))
+  return(bd)
 }

44c67d761ded8aea001ba0693f9333316e437e07
EAMI91 <emiliomorones@gmail.com>
2022-04-22 17:54:34 -0500

    nuevo esquema

--- R/lectura.R
+++ R/lectura.R
@@ -15,38 +15,14 @@
 #' @examples
 #'
 leer_base <- function(eleccion, entidad){
-
+  estado <- if_else(grepl("_df_|_pr_",eleccion), "nac",entidad)
+  res <- readr::read_rds(system.file(glue::glue("electoral/{estado}_{eleccion}.rda"),
+                                     package = "aelectoral2",
+                                     mustWork = TRUE))
+  if(estado == "nac") {
+    nombre <- diccionario %>% filter(abreviatura == !!entidad) %>% pull(estado)
+    res <- res %>% filter(estado == !!nombre)
+    }
   return(res)
 }
 
-limpiar_base <- function(bd){
-
-  bd <- bd %>% map(~{
-    aux <- .x %>%
-      select(-matches(basura$columna)) %>%
-      mutate(across(any_of(geograficas$columna), ~as.character(.x))) #variables geográficas character
-    aux %>%
-      mutate(across(all_of(names(aux)[is.na(match(names(aux), geograficas$columna))]), ~as.numeric(.x))) %>%
-      mutate(across(where(is.character),
-                    ~stringr::str_replace_all(string = .x,pattern = "\\n",replacement = " ")))# quitar enters
-  })
-
-  return(bd)
-}
-
-revisar_nombres <- function(bd){
-  bd %>% imap(~{
-    nombres <- .x %>% select(-contains("independiente")) %>% #en el caso de independientes sí pueden haber números
-      select(matches("[[:digit:]]")) %>% names
-    if(length(nombres)>0) warning(glue::glue("Base: {.y} \n Revisar las columnas {paste(nombres, collapse = ', ')} \n\n Las columnas que tienen números es porque los nombres están repetidos y readr les agrega el número de la columna.
-                                         \n Esto es por un error manual y lo deberá arreglar Andrés."))
-  })
-
-}
-
-sufijo <- function(bd, nombre, geo){
-  aux <- nombre %>% str_split("/") %>% pluck(1)
-  s <- glue::glue("{substr(aux[3],1,1)}{substr(aux[1],1,1)}_{substr(aux[2],3,4)}") %>% tolower
-  bd <- bd %>% rename_at(vars(-any_of(geo)), function(x) paste(x, s, sep = "_"))
-  return(bd)
-}

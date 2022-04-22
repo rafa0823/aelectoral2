bbebab6a0f59562fd8fa77430f3d3e6117efad75
EAMI91 <emiliomorones@gmail.com>
2022-04-22 16:52:38 -0500

    intento de hacer la repartición de coalición

--- data-raw/ej_test.R
+++ data-raw/ej_test.R
@@ -11,6 +11,8 @@ t_nac(pr18, "candidato", "morena_|pt_|pes_")
 #partido
 t_nac(pr18, "partido", "total_")
 t_nac(pr18, "partido", "morena_")
+t_nac(pr18, "partido", "pt_")
+t_nac(pr18, "partido", "pes_")
 
 # Niveles -----------------------------------------------------------------
 
@@ -30,5 +32,10 @@ cas <- pr18 %>% sample_n(1) %>% pull(clave_casilla)
 t_nivel(pr18, "candidato", "ele_morena_pr_18", estado = 15, nivel = "clave_casilla", seleccion = cas)
 
 
-
+pr18 %>% group_by(distritof) %>%
+  summarise(across(starts_with("ele_"), ~sum(.x,na.rm = T))) %>%
+  select(distritof, matches("morena_|pt_|pes_")) %>%
+  pivot_longer(-distritof) %>% mutate(partidos = str_count(name,"_")-2) %>%
+  mutate(cociente = value %/% partidos,
+         residuo = value %% partidos)
 

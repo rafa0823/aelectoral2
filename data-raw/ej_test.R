c0b078ef558dfbc015d7bddc41d780daf8be1d36
EAMI91 <emiliomorones@gmail.com>
2022-04-25 16:10:06 -0500

    repartir coalición. historia #AEEB-17

--- data-raw/ej_test.R
+++ data-raw/ej_test.R
@@ -1,8 +1,8 @@
 library(readr)
 library(tidyr)
 load("data/nac_pr_18.rda")
-pr18 <- nac_pr_18 %>% as_tibble %>% mutate(across(starts_with("ele_"), ~as.numeric(.x)))
-
+pr18 <- readr::read_rds("inst/electoral/nac_pr_18.rda") %>% as_tibble %>%
+  mutate(distritof_18 = paste(estado,distritof_18))
 
 # Nacional ----------------------------------------------------------------
 
@@ -10,9 +10,13 @@ pr18 <- nac_pr_18 %>% as_tibble %>% mutate(across(starts_with("ele_"), ~as.numer
 t_nac(pr18, "candidato", "morena_|pt_|pes_")
 #partido
 t_nac(pr18, "partido", "total_")
-t_nac(pr18, "partido", "morena_")
-t_nac(pr18, "partido", "pt_")
-t_nac(pr18, "partido", "pes_")
+
+pr18_partido <- pr18 %>% repartir_coalicion(nivel = "distritof_18", eleccion = "pr_18")
+
+
+t_nac(pr18_partido, "partido", "morena_")
+t_nac(pr18_partido, "partido", "pt_")
+t_nac(pr18_partido, "partido", "pes_")
 
 # Niveles -----------------------------------------------------------------
 

9545f14c91e0f7dfde184dc1784d3aebaefcbcfc
Déborah Martínez <deborahandrea.m@hotmail.com>
2022-04-25 11:17:00 -0500

    test de conjunta

--- data-raw/ejemplo.R
+++ data-raw/ejemplo.R
@@ -1,10 +1,23 @@
 devtools::load_all()
 # devtools::install()
 # library(aelectoral2)
-bd <- Electoral$new("df_21", entidad = "mex", extranjero = F)
+library(purrr)
+library(dplyr)
+library(tidyr)
+library(stringr)
 
-bd$agregar_bd("df_18", entidad = "mex")
-bd$agregar_bd("pr_18", entidad = "mex")
-bd$bd %>% nrow
+mex <- Electoral$new(eleccion = "df_21", entidad = "mex",extranjero = T,especiales = T)
 
 
+c("df_18", "df_15", "pr_18", "pm_21", "pm_18", "gb_17", "dl_18") %>%
+  walk(~{
+    mex$agregar_bd(eleccion = .x, entidad = "mex")
+  })
+
+
+
+mex$bd %>%  t_nivel("partido", "ele_morena_df_21", estado = 15,
+                    nivel = "seccion",  seleccion = "5934")
+
+mex$bd %>%  t_nivel("partido", "ele_mc_df_21", estado = 15,
+                    nivel = "seccion",  seleccion = "0392")

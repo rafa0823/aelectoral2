7c5c29a846b8a4475e04c893c4c687f2a3bb36a5
EAMI91 <emiliomorones@gmail.com>
2022-04-26 18:05:47 -0500

    repartir coalición junto con candidatura comun

--- R/partidos.R
+++ R/partidos.R
@@ -1,6 +1,4 @@
 repartir_coalicion <- function(bd, nivel, eleccion){
-
-
   aux <- bd %>% group_by(across(all_of(nivel))) %>%
     summarise(across(starts_with("ele_"), ~sum(.x,na.rm = T))) %>%
     filter(!is.na(!!rlang::sym(nivel))) %>%
@@ -9,8 +7,8 @@ repartir_coalicion <- function(bd, nivel, eleccion){
 
   division <- aux %>%
     pivot_longer(-nivel) %>% mutate(
-      num_partidos = str_count(name, "_") - 2,
-      partidos = str_split(gsub(pattern = glue::glue("ele_|_{eleccion}"),"",name), "_"),
+      partidos = stringr::str_split(gsub(pattern = glue::glue("ele_|_{eleccion}|_cc"),"",name), "_"),
+      num_partidos = purrr::map_int(partidos,~length(.x)),
       partido = value %/% num_partidos,
       residuo = value %% num_partidos
     )
@@ -38,6 +36,52 @@ repartir_coalicion <- function(bd, nivel, eleccion){
     }) %>% bind_rows()
 
   total <- total %>% select(all_of(nivel), name, partido) %>%
+    tidyr::pivot_wider(names_from = "name", values_from = "partido")
+
+  return(total)
+}
+
+repartir_coalicion2 <- function(bd, nivel, eleccion){
+
+  aux <- bd %>% group_by(across(all_of(nivel))) %>%
+    summarise(across(starts_with("ele_"), ~sum(.x,na.rm = T))) %>%
+    filter(!is.na(!!rlang::sym(nivel))) %>%
+    select(-contains("total"),-contains("nominal")) %>%
+    select(all_of(nivel), contains(eleccion))
+
+  division <- aux %>%
+    pivot_longer(-nivel) %>% mutate(
+      partidos = stringr::str_split(gsub(pattern = glue::glue("ele_|_{eleccion}|_cc"),"",name), "_"),
+      num_partidos = purrr::map_int(partidos,~length(.x)),
+      partido = value %/% num_partidos,
+      residuo = value %% num_partidos
+    )
+
+  total <- division %>% group_by(across(all_of(nivel))) %>% tidyr::nest() %>%
+    mutate(d2 = purrr::map(data, ~{
+      partidos <- .x %>% filter(num_partidos == 1) %>%
+        tidyr::unnest(partidos) %>%
+        select(-residuo)
+      alianzas <- .x %>% filter(num_partidos > 1)
+
+
+
+      for(i in seq_len(nrow(alianzas))){
+        al <- alianzas %>% slice(i)
+        modif <- partidos %>%
+          filter(partidos %in% (al$partidos %>% purrr::pluck(1))) %>%
+          mutate(ranking = dense_rank(-partido),
+                 partido = partido + al$partido + (ranking <= al$residuo)
+          ) %>% select(-ranking)
+        partidos <- partidos %>% anti_join(modif, by = c("name")) %>% bind_rows(modif)
+
+      }
+
+      return(partidos)
+    })
+    )
+
+  total <- total %>% select(-data) %>% tidyr::unnest(d2) %>% select(all_of(nivel), name, partido) %>%
     tidyr::pivot_wider(names_from = "name", values_from = "partido")
 
   return(total)

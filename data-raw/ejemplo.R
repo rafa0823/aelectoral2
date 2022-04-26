74e419893942f205547523189c6089e934cfd4c4
EAMI91 <emiliomorones@gmail.com>
2022-04-26 11:46:54 -0500

    forma de agregar bases de datos manuales. Historia #AEEB-23

--- data-raw/ejemplo.R
+++ data-raw/ejemplo.R
@@ -4,5 +4,34 @@ devtools::load_all()
 library(tidyr)
 bd <- Electoral$new("df_21", entidad = "mex", extranjero = T)
 
-bd$agregar_bd("df_18", entidad = "mex",llaves = c("seccion", "distritof", "distritol"))
-bd$agregar_bd("pr_18", entidad = "mex")
+
+bd$agregar_bd("df_18", entidad = "mex",llaves = c("seccion", "distritof", "distritol", "municipio"))
+bd$agregar_bd("pm_21", entidad = "mex")
+
+
+bd$agregar_bd("cp_22", entidad = "mex")
+
+data(cat_utm_22)
+cat <- cat_utm_22 %>% filter(estado == 15) %>%
+  distinct(seccion, unidad_territorial, sede)
+
+bd$bd %>% nrow
+bd$agregar_manual(cat, by = "seccion")
+bd$bd %>% nrow
+
+# Agregar regiones --------------------------------------------------------
+data(regiones)
+reg <- regiones %>% select(region, municipio)
+
+# reg %>% anti_join(bd$bd, by = c("municipio" = "nombre_municipio_pm_21"))
+bd$agregar_manual(reg, by = c("nombre_municipio_pm_21" = "municipio"))
+
+# Agregar presidentes municipales -----------------------------------------
+
+data("presidentes_mpos_mex")
+presidentes <- presidentes_mpos_mex %>% select(1:3) %>%
+  mutate(nombre_municipio = stringr::str_replace(nombre_municipio,"CASTANEDA", "CASTAÑEDA"))
+
+presidentes %>% anti_join(bd$bd, by = c("nombre_municipio" = "nombre_municipio_pm_21"))
+bd$agregar_manual(presidentes, by = c("nombre_municipio_pm_21" = "nombre_municipio"))
+nrow(bd$bd)

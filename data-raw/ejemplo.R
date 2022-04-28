a8f900dfa396c3bb0906fd13a6531252c274d729
EAMI91 <emiliomorones@gmail.com>
2022-04-28 14:08:22 -0500

    repartir candidato. historia #AEEB-17

--- data-raw/ejemplo.R
+++ data-raw/ejemplo.R
@@ -10,21 +10,15 @@ bd$agregar_bd("df_18", entidad = "mex",llaves = c("seccion", "distritof", "distr
 
 # Agregar pm_21 con extraordinaria ----------------------------------------
 bd$agregar_bd("pm_21", entidad = "mex",extraordinaria = c(eleccion = "pmext_21", entidad = "mex"))
-
-bd$bd %>% names
-bd$bd %>% nrow
 # Agregar consulta popular 22 ---------------------------------------------
 
 
 bd$agregar_bd("cp_22", entidad = "mex")
-
 data(cat_utm_22)
 cat <- cat_utm_22 %>% filter(estado == 15) %>%
   distinct(seccion, unidad_territorial, sede)
 
-bd$bd %>% nrow
 bd$agregar_manual(cat, by = "seccion")
-bd$bd %>% nrow
 
 # Agregar regiones --------------------------------------------------------
 data(regiones)
@@ -43,4 +37,23 @@ presidentes %>% anti_join(bd$bd, by = c("nombre_municipio" = "nombre_municipio_p
 bd$agregar_manual(presidentes, by = c("nombre_municipio_pm_21" = "nombre_municipio"))
 nrow(bd$bd)
 
+
+# Repartir coalicion partido ----------------------------------------------
+
+bd$partido(nivel = "distritof_21",eleccion = "df_21")
+bd$partido(nivel = "distritof_18",eleccion = "df_18")
+
+bd$bd_partido$df_21
+bd$bd_partido$df_18
+
+
+# Coalición candidato -----------------------------------------------------
+
+alianzas_df18 <- readr::read_csv("data-raw/df_18_edomex.csv") %>%
+  mutate(distritof_18 = formatC(distritof_18, width = 2, flag = 0))
+
+undebug(repartir_candidato)
+bd$candidato(alianzas_df18,nivel = "distritof_18", "df_18")
+
+bd$bd_candidato
 

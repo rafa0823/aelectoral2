9f834d9ad9c5958a4c124094e314929b83e2f8c0
EAMI91 <emiliomorones@gmail.com>
2022-05-02 18:26:45 -0500

    data-raw cartografía. historia #AEEB-21

---
+++ data-raw/cartografia.R
@@ -0,0 +1,37 @@
+library(dplyr)
+library(purrr)
+wd <- "/Users/emiliomorones/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/INE/SHP/2021"
+
+
+# Distrito Federal --------------------------------------------------------
+
+wd %>% list.files(full.names = T) %>% map(~{
+  list.files(.x,pattern = "DISTRITO_FEDERAL", full.names = T)
+}) %>% flatten() %>% do.call(c,.) %>% map(~{
+
+  aux <- substr(.x,112,113)
+  terminacion <- substr(.x, nchar(.x)-2, nchar(.x))
+
+    file.copy(.x,
+            glue::glue("~/Documents/Git/aelectoral2/inst/shp/df_21/{aux}.{terminacion}")
+  )
+
+})
+
+
+# Distrito Local ----------------------------------------------------------
+
+wd %>% list.files(full.names = T) %>% map(~{
+  list.files(.x,pattern = "DISTRITO_LOCAL", full.names = T)
+}) %>% flatten() %>% do.call(c,.) %>% map(~{
+
+  aux <- substr(.x,112,113)
+  terminacion <- substr(.x, nchar(.x)-2, nchar(.x))
+
+  file.copy(.x,
+            glue::glue("~/Documents/Git/aelectoral2/inst/shp/dl21/{aux}.{terminacion}")
+  )
+
+})
+
+

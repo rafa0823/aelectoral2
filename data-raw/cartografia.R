484e32ea1a98fecfd3a27f2b5675faaafb3ad5ec
EAMI91 <emiliomorones@gmail.com>
2022-05-03 18:17:33 -0500

    arreglé variables y cartografía por sección. historia #AEEB-21

--- data-raw/cartografia.R
+++ data-raw/cartografia.R
@@ -6,15 +6,16 @@ wd <- "/Users/emiliomorones/Dropbox (Selva)/Ciencia de datos/Consultoría Estad
 # Distrito Federal --------------------------------------------------------
 
 wd %>% list.files(full.names = T) %>% map(~{
-  list.files(.x,pattern = "DISTRITO_FEDERAL", full.names = T)
-}) %>% flatten() %>% do.call(c,.) %>% map(~{
+  list.files(.x,pattern = "DISTRITO_FEDERAL.shp", full.names = T)
+})%>% do.call(c,.) %>% map(~{
 
   aux <- substr(.x,112,113)
   terminacion <- substr(.x, nchar(.x)-2, nchar(.x))
 
-    file.copy(.x,
-            glue::glue("~/Documents/Git/aelectoral2/inst/shp/df_21/{aux}.{terminacion}")
-  )
+  temp <- st_read(.x) %>% transmute(distritof_21 = paste(formatC(ENTIDAD, width = 2, flag = 0),
+                                                         formatC(DISTRITO_F, width = 2, flag = 0), sep = "_"))
+  readr::write_rds(temp,glue::glue("inst/shp/df_21/{aux}.rda"))
+
 
 })
 
@@ -22,30 +23,31 @@ wd %>% list.files(full.names = T) %>% map(~{
 # Distrito Local ----------------------------------------------------------
 
 wd %>% list.files(full.names = T) %>% map(~{
-  list.files(.x,pattern = "DISTRITO_LOCAL", full.names = T)
+  list.files(.x,pattern = "DISTRITO_LOCAL.shp", full.names = T)
 }) %>% flatten() %>% do.call(c,.) %>% map(~{
 
   aux <- substr(.x,112,113)
   terminacion <- substr(.x, nchar(.x)-2, nchar(.x))
 
-  file.copy(.x,
-            glue::glue("~/Documents/Git/aelectoral2/inst/shp/dl_21/{aux}.{terminacion}")
-  )
+  temp <- st_read(.x) %>% transmute(distritol_21 = paste(formatC(ENTIDAD, width = 2, flag = 0),
+                                                         formatC(DISTRITO_L, width = 2, flag = 0), sep = "_"))
+  readr::write_rds(temp,glue::glue("inst/shp/dl_21/{aux}.rda"))
 
 })
 
 # Municipio ---------------------------------------------------------------
 
 wd %>% list.files(full.names = T) %>% map(~{
-  list.files(.x,pattern = "MUNICIPIO", full.names = T)
+  list.files(.x,pattern = "MUNICIPIO.shp", full.names = T)
 }) %>% flatten() %>% do.call(c,.) %>% map(~{
 
   aux <- substr(.x,112,113)
   terminacion <- substr(.x, nchar(.x)-2, nchar(.x))
 
-  file.copy(.x,
-            glue::glue("~/Documents/Git/aelectoral2/inst/shp/pm_21/{aux}.{terminacion}")
-  )
+  temp <- st_read(.x) %>% transmute(municipio_21 = paste(formatC(ENTIDAD, width = 2, flag = 0),
+                                                         formatC(MUNICIPIO, width = 3, flag = 0), sep = "_"),
+                                    nombre_municipio_21 = NOMBRE)
+  readr::write_rds(temp,glue::glue("inst/shp/mun_21/{aux}.rda"))
 
 })
 
@@ -53,18 +55,45 @@ wd %>% list.files(full.names = T) %>% map(~{
 # Estados -----------------------------------------------------------------
 
 wd %>% list.files(full.names = T) %>% map(~{
-  list.files(.x,pattern = "ENTIDAD", full.names = T)
+  list.files(.x,pattern = "ENTIDAD.shp", full.names = T)
 }) %>% flatten() %>% do.call(c,.) %>% map(~{
 
   aux <- substr(.x,112,113)
   terminacion <- substr(.x, nchar(.x)-2, nchar(.x))
 
-  file.copy(.x,
-            glue::glue("~/Documents/Git/aelectoral2/inst/shp/gb_21/{aux}.{terminacion}")
-  )
+  temp <- st_read(.x) %>% transmute(entidad = formatC(ENTIDAD, width = 2, flag = 0),
+                                    nombre_entidad = NOMBRE,
+                                    circunscripcion = CIRCUNSCRI)
+
+  readr::write_rds(temp,glue::glue("inst/shp/ent_21/{aux}.rda"))
 
 })
 
 
+# Sección -----------------------------------------------------------------
+
+# Estados -----------------------------------------------------------------
+
+wd %>% list.files(full.names = T) %>% map(~{
+  list.files(.x,pattern = "SECCION.shp", full.names = T)
+}) %>% flatten() %>% do.call(c,.) %>% map(~{
+
+  aux <- substr(.x,112,113)
+  terminacion <- substr(.x, nchar(.x)-2, nchar(.x))
+
+  temp <- st_read(.x) %>% transmute(entidad = formatC(ENTIDAD, width = 2, flag = 0),
+                                    distritof_21 = paste(formatC(ENTIDAD, width = 2, flag = 0),
+                                                         formatC(DISTRITO, width = 2, flag = 0), sep = "_"),
+                                    distritol_21 = paste(formatC(ENTIDAD, width = 2, flag = 0),
+                                                         formatC(DISTRITO_L, width = 2, flag = 0), sep = "_"),
+                                    municipio_21 = paste(formatC(ENTIDAD, width = 2, flag = 0),
+                                                         formatC(MUNICIPIO, width = 3, flag = 0), sep = "_"),
+                                    seccion = paste(formatC(ENTIDAD, width = 2, flag = 0),
+                                                         formatC(SECCION, width = 4, flag = 0), sep = "_")
 
 
+                                    )
+
+  readr::write_rds(temp,glue::glue("inst/shp/secc_21/{aux}.rda"))
+
+})

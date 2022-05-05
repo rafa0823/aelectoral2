598b22c2c33b93b40bcdd33c6d4d367aef4071ab
datos-morant <datosmorant@gmail.com>
2022-05-05 14:58:54 -0500

    alianzas nacionales

--- data-raw/bdos_dgo_auxiliares.R
+++ data-raw/bdos_dgo_auxiliares.R
@@ -11,7 +11,7 @@ alianzas_dgo_pm_19 <- read.csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría
          nombre_estado = "DURANGO",
          municipio_19 = formatC(municipio_19, width = 2, flag = "0"))
 
-alianzas_dgo_pm_19 %>% write_rds("inst/bdos_auxiliares_dgo//alianzas_dgo_pm_19.rda")
+alianzas_dgo_pm_19 %>% write_rds("inst/alianzas/alianzas_durango/alianzas_dgo_pm_19.rda")
 
 alianzas_dgo_pm_16 <- read.csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/alianzas/locales/pm_16_durango.csv") %>%
   janitor::clean_names() %>%
@@ -20,16 +20,17 @@ alianzas_dgo_pm_16 <- read.csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría
          nombre_estado = "DURANGO",
          municipio_16 = formatC(municipio_16, width = 2, flag = "0"))
 
-alianzas_dgo_pm_16 %>% write_rds("inst/bdos_auxiliares_dgo//alianzas_dgo_pm_16.rda")
+alianzas_dgo_pm_16 %>% write_rds("inst/alianzas/alianzas_durango/alianzas_dgo_pm_16.rda")
 
 alianzas_dgo_gb_16 <- read.csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/alianzas/locales/gob_16_durango.csv") %>%
   janitor::clean_names() %>%
   as_tibble() %>%
   mutate(estado = "10",
          nombre_estado = "DURANGO",
-         municipio_16 = formatC(municipio_16, width = 2, flag = "0"))
+         municipio_16 = formatC(municipio_16, width = 2, flag = "0"),
+         eleccion = "gb_16")
 
-alianzas_dgo_gb_16 %>% write_rds("inst/bdos_auxiliares_dgo//alianzas_dgo_gb_16.rda")
+alianzas_dgo_gb_16 %>% write_rds("inst/alianzas/alianzas_durango/alianzas_dgo_gb_16.rda")
 
 
 

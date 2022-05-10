e7b76820a14164203ad5833c451a7c6c6363ea04
datos-morant <datosmorant@gmail.com>
2022-05-10 14:37:32 -0500

    locales coahuila script

--- data-raw/bdos_coah_locales.R
+++ data-raw/bdos_coah_locales.R
@@ -15,7 +15,7 @@ bd_pm_18_coah <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Esta
   janitor::clean_names() %>%
   as_tibble()
 
-bd_gb_17_coah <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/Resultados definitivos/Local/2017/Gobernador/coahuila_normal_seccion.csv") %>%
+bd_gb_17_coah <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/Resultados definitivos/Local/2017/Gobernador/coah_normal_casilla.csv") %>%
   janitor::clean_names() %>%
   as_tibble()
 
@@ -60,8 +60,6 @@ pm21 <- pm21 %>%
          distritol_21 = if_else(seccion == "000", "00", distritol_21))
 
 
-
-
 pm21 <- pm21 %>%
   rename_with.(~paste0('ele_', .x),
                .cols = pan:nominal)
@@ -80,12 +78,14 @@ final_pm21_coah <- final_pm21_coah  %>%
                                    nchar(tipo) == 5 ~ gsub(pattern = "E1C","01", tipo)),
 
 
-         estado = "10",
-         nombre_estado = "DURANGO",
+         estado = "05",
+         nombre_estado = "COAHUILA",
          tipo_casilla = substr(tipo,1,1),
          clave_casilla = paste0(estado,seccion,tipo_casilla,clave_casilla))
 
 final_pm21_coah %>% count(tipo) %>% view()
+
+final_pm21_coah %>% count(nchar(clave_casilla))
 
 
 # guardar rda
@@ -97,112 +97,60 @@ coah_pm_21 %>% write_rds("inst/electoral/coah_pm_21.rda")
 rm(pm21)
 
 
-## gb 16 DURANGO  ------------------------------------------------------------------------------------------------------
+## DL 20 COAHUILA  ------------------------------------------------------------------------------------------------------
 
-gb16 <- bd_gb_16_coah   %>%
-  mutate(nombre_municipio = gsub(pattern = "( |)[0-9]",replacement = "",x = nombre_municipio),
-         pan = 0)
+dl20 <- bd_dl_20_coah
 
 # revisar nombres de varianles
 
-colnames(gb16)
+colnames(dl20)
 
-gb16 <- gb16 %>%
-  rename("noreg"= no_reg,
-         "municipio_16" = municipio,
-         "nombre_municipio_16" = nombre_municipio,
-         distritol_16 = distrito,
-         nombre_distritol_16 = cabecera_distrital,
-         pes = es
+dl20 <- dl20 %>%
+  rename("noreg"= cand_nreg,
+         total = total_votos,
+         nominal = lista,
+         "municipio_20" = municipio,
+         nombre_municipio_20 = nom_mun,
+         distritol_20 = dtto_loc,
+         distritof_20 = dtto_f
   )%>%
-  mutate(across(pan_prd:pan, ~as.numeric(.x)),
+  select(cve_cas2:total,nominal,clasificacion,bol_ent,urna_e) %>%
+  mutate(across(pan:nominal, ~as.numeric(.x)),
          seccion = formatC(seccion, width = 4,flag = "0"),
          seccion = if_else(casilla == "P","9999",seccion),
-         municipio_16 = formatC(municipio_16, width = 3, flag = "0"),
-         distritol_16 = formatC(distritol_16, width = 2, flag = "0"))
+         municipio_20 = formatC(municipio_20, width = 3, flag = "0"),
+         distritol_20 = formatC(distritol_20, width = 2, flag = "0"),
+         distritof_20 = formatC(distritof_20, width = 2, flag = "0"))
 
 
-gb16 <- gb16 %>%
+dl20 <- dl20 %>%
   rename_with.(~paste0('ele_', .x),
-               .cols = pan_prd:pan)
+               .cols = pan:nominal)
 
 # Identificar los partidos de la elecccion
-detectar_partidos(gb16)
+detectar_partidos(dl20)
 
 # sufijo para join
 
-final_gb16_coah <- insertar_sufijo(bd=gb16, "gb", "16")
+final_dl20_coah <- insertar_sufijo(bd=dl20, "dl", "20")
 
-final_gb16_coah <- final_gb16_coah  %>%
-  mutate(casilla = formatC(casilla, width = 2,flag = "0"),
-         tipo_casilla = "X",
-         estado = "10",
-         nombre_estado = "DURANGO",
-         clave_casilla = paste0(estado,seccion,tipo_casilla,casilla,"XX"))
+final_dl20_coah <- final_dl20_coah  %>%
+  mutate(tipo_casilla = substr(tipo,1,1),
+         id_casilla = case_when(nchar(tipo) == 2 ~ paste0(gsub("[[:alpha:]]","0",tipo),"00"),
+                                nchar(tipo) == 3 ~ paste0(gsub("[[:alpha:]]","",tipo),"00"),
+                                nchar(tipo) == 4 ~ gsub("[[:alpha:]]","0",tipo),
+                                nchar(tipo) == 5 ~ paste0("0",gsub("[[:alpha:]]","",tipo))),
+         id_casilla = if_else(id_casilla == "100","0100",id_casilla),
+         estado = "05",
+         nombre_estado = "COAHUILA",
+         clave_casilla = paste0(estado,seccion,tipo_casilla,id_casilla))
 
 
 # guardar rda
 
-coah_gb_16 <- final_gb16_coah
+coah_dl_20 <- final_dl20_coah
 
-coah_gb_16 %>% write_rds("inst/electoral/coah_gb_16.rda")
+coah_dl_20 %>% write_rds("inst/electoral/coah_dl_20.rda")
 
-rm(gb16)
+rm(dl20)
 
-## PM 16 DURANGO ------------------------------------------------------------------------------------------------------
-
-pm16 <- bd_pm_16_coah   %>%
-  mutate(nombre_municipio = gsub(pattern = "( |)[0-9]",replacement = "",x = nombre_municipio),
-         pan = 0)
-
-
-# revisar nombres de varianles
-
-colnames(pm16)
-
-pm16 <- pm16 %>%
-  rename("noreg"= no_reg,
-         "municipio_16" = municipio,
-         "nombre_municipio_16" = nombre_municipio,
-         distritol_16 = distrito,
-         nombre_distritol_16 = nombre_distrito,
-  )%>%
-  mutate(across(pan_prd:pan, ~as.numeric(.x)),
-         seccion = formatC(seccion, width = 4,flag = "0"),
-         seccion = if_else(casilla == "P","9999",seccion),
-         municipio_16 = formatC(municipio_16, width = 3, flag = "0"),
-         distritol_16 = formatC(distritol_16, width = 2, flag = "0"))
-
-
-pm16 <- pm16 %>%
-  rename_with.(~paste0('ele_', .x),
-               .cols = pan_prd:pan)
-
-# Identificar los partidos de la elecccion
-detectar_partidos(pm16)
-
-# sufijo para join
-
-final_pm16_coah <- insertar_sufijo(bd=pm16, "pm", "16")
-
-final_pm16_coah <- final_pm16_coah  %>%
-  mutate(clave_casilla = case_when(nchar(casilla) == 1 ~ paste0(casilla,"0100"),
-                                   nchar(casilla) == 3 ~ paste0(casilla,"00"),
-                                   nchar(casilla) == 6 ~ gsub(pattern = "C","",casilla),
-                                   nchar(casilla) == 5 ~ paste0(gsub(pattern = "[MR]","",casilla),"00")),
-         mr_rp = if_else(nchar(casilla) == 5, gsub(pattern = "[[:digit:]]","",casilla),""),
-         mr_rp = gsub(pattern = "S","",mr_rp),
-         mr_rp = if_else(mr_rp == "","MR",mr_rp),
-         estado = "10",
-         nombre_estado = "DURANGO",
-         tipo_casilla = substr(casilla,1,1),
-         clave_casilla = paste0(estado,seccion,clave_casilla))
-
-
-# guardar rda
-
-coah_pm_16 <- final_pm16_coah
-
-coah_pm_16 %>% write_rds("inst/electoral/coah_pm_16.rda")
-
-rm(pm16)

28b77e5662a6a1b58e8c360147d8de27eb666388
EAMI91 <emiliomorones@gmail.com>
2022-05-18 14:49:09 -0500

    print clase ElectoralSHP. historia #AEEB-57

--- R/clases.R
+++ R/clases.R
@@ -219,7 +219,7 @@ Criterio de casillas especiales: {if(is.null(self$especiales)) 'ninguna acción 
 ElectoralSHP <- R6::R6Class("ElectoralSHP",
                             public = list(
                               shp = list(),
-                              entidad = NULL,
+                              entidades = NULL,
                               #'@description
                               #'Lee un shapefile
                               #' @param unidad si es de municipio, estado, distrito, seccion, etc.
@@ -228,13 +228,17 @@ ElectoralSHP <- R6::R6Class("ElectoralSHP",
                               #' @return Una lista con shapefiles
                               #' @examples
                               initialize = function(unidad, entidad){
-                                self$entidad <- entidad
+                                self$entidades <- entidad
                                 aux <- leer_shp(unidad, entidad)
                                 self$shp <- self$shp %>% append(list(aux) %>% purrr::set_names(paste(unidad, entidad, sep = "_")))
                               },
-                              agregar_shp = function(unidad){
-                                aux <- leer_shp(unidad, self$entidad)
-                                self$shp <- self$shp %>% append(list(aux) %>% purrr::set_names(paste(unidad, self$entidad, sep = "_")))
+                              print = function(){
+                                cat(glue::glue("Entidad(es): {paste(self$entidades, collapse = ', ')} \n\n Shps agregados: {paste(names(self$shp), collapse = ', ')}"))
+                              },
+                              agregar_shp = function(unidad, entidad = NULL){
+                                if(!entidad %in% self$entidades) self$entidades <- self$entidades %>% append(entidad)
+                                aux <- leer_shp(unidad, entidad)
+                                self$shp <- self$shp %>% append(list(aux) %>% purrr::set_names(paste(unidad, entidad, sep = "_")))
                               },
                               #'@description
                               #' Junta shapefiles

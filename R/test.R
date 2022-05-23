e5bfbcc78297979b8665a3c029a065dcd749b1c9
Déborah Martínez <deborahandrea.m@hotmail.com>
2022-05-23 12:37:54 -0500

    Documentación de los test

--- R/test.R
+++ R/test.R
@@ -1,13 +1,13 @@
-#' Title
+#' Te trae el resultado de votos de una variable seleccionada con un patron de una bd electoral.
 #'
-#' @param bd
-#' @param tipo
-#' @param patron
+#' @param bd Base de datos a testear
+#' @param tipo si es por partido o por candidato
+#' @param patron la variable de la que se quiere el resultado
 #'
-#' @return
-#' @export
-#'
+#' @return El resultado electoral con el método señalado
 #' @examples
+#' aux <- mex$bd %>% repartir_coalicion(nivel = "distritof_21", eleccion = "df_21")
+#' t_nac(aux, "partido", "morena_")
 t_nac <- function(bd, tipo, patron){
   tipo <- match.arg(tipo, c("candidato","partido"))
 
@@ -30,19 +30,17 @@ t_nac <- function(bd, tipo, patron){
   return(res)
 }
 
-#' Title
+#' Devuelve el resultado electoral por partido o por candidato de una variable seleccionada con un patron de un estado en particular en un nivel seleccionado
 #'
-#' @param bd
-#' @param tipo
-#' @param patron
-#' @param estado
+#' @param bd Base de datos a testear
+#' @param tipo si es por partido o por candidato
+#' @param patron la variable de la que se quiere el resultado
+#' @param estado Número de la entidad del ine
 #' @param nivel Nivel en el que se determinan las alianzas dependiendo de la unidad en la que se realiza la elección.
-#' @param seleccion
+#' @param seleccion que selección de acuerdo con el nivel se requiere
 #'
-#' @return
-#' @export
-#'
-#' @examples
+#' @return  Resultado electoral por nivel
+#' @examples mex$bd %>%  t_nivel("candidato", "ele_morena_dl_21", estado = 15, nivel = "estado",  seleccion = "15")
 t_nivel <- function(bd, tipo, patron, estado, nivel, seleccion){
   tipo <- match.arg(tipo, c("candidato","partido"))
 

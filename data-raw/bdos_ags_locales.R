c44775b4af5decedcf8826bb03c0341ff72b1a2e
datos-morant <datosmorant@gmail.com>
2022-06-14 17:58:42 -0500

    bases electoral hidalgo y ags

--- data-raw/bdos_ags_locales.R
+++ data-raw/bdos_ags_locales.R
@@ -81,6 +81,12 @@ bd_pm_16_ags <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estad
   janitor::clean_names() %>%
   as_tibble()
 
+# dl 16
+
+bd_dl_16_ags <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/Resultados definitivos/Local/2016/Distrito local/aguascalientes_normal_casilla.csv") %>%
+  janitor::clean_names() %>%
+  as_tibble()
+
 
 ## GB 16 AGUASCALIENTES ------------------------------------------------------------------------------------------------------
 
@@ -481,5 +487,76 @@ ags_pm_16 <- final_pm16_ags
 ags_pm_16 %>% write_rds("inst/electoral/ags/pm_16.rda")
 
 rm(pm16)
+
+## dl 16 AGUASCALIENTES ------------
+
+
+bd_dl_16_ags %>% colnames
+
+dl16 <- bd_dl_16_ags   %>%
+  mutate(municipio = gsub(pattern = "( |)[0-9]",replacement = "",x = municipio))%>%
+  select(!validos)
+
+# revisar nombres de varianles
+
+colnames(dl16)
+
+dl16 <- dl16 %>%
+  rename(noreg = no_reg,
+         distritol_16 = id_distrito,
+         nombre_distritol_16 = cabecera_distrital,
+         municipio_16 = municipio,
+         nombre_municipio_16 = nombre_municipio
+  )%>%
+  rename_with( ~ gsub("independiente_", "independiente", .x, fixed = TRUE)) %>%
+  mutate(across(pan:nominal, ~as.numeric(.x)),
+         seccion = formatC(seccion, width = 4,flag = "0"),
+         seccion = if_else(casilla == "P","9999",seccion),
+         municipio_16 = formatC(municipio_16, width = 3, flag = "0"),
+         distritol_16 = formatC(distritol_16, width = 3, flag = "0"))
+
+
+dl16 <- dl16 %>%
+  rename_with.(~paste0('ele_', .x),
+               .cols = pan:nominal)
+
+# Identificar los partidos de la elecccion
+detectar_partidos(dl16)
+
+# sufijo para join
+
+final_dl16_ags <- insertar_sufijo(bd=dl16, "dl", "16")
+
+#agregar clave casillas
+
+
+final_dl16_ags <- final_dl16_ags  %>%
+  mutate(id_casilla = case_when(nchar(casilla) == 1 ~ paste0(casilla,"0100"),
+                                nchar(casilla) == 3 ~ paste0(casilla,"00"),
+                                nchar(casilla) == 6 ~ gsub(pattern = "C","",casilla),
+                                nchar(casilla) == 5 ~ paste0("S",substr(casilla,4,5),"00")),
+
+         estado = "01",
+         nombre_estado = "AGUASCALIENTES",
+         tipo_casilla = substr(casilla,1,1),
+         clave_casilla = paste0(estado,seccion,id_casilla))
+
+#pruebas
+
+final_dl16_ags %>% count(nchar(clave_casilla))
+final_dl16_ags %>% count(id_casilla)
+
+# guardar rda
+
+ags_dl_16 <- final_dl16_ags
+
+ags_dl_16 %>% write_rds("inst/electoral/ags/dl_16.rda")
+
+rm(dl16)
+
+
+
+
+
 
 

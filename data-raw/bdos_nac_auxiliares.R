ea0d7ce8a0eaede6fb2a2d666837230244bf064b
datos-morant <datosmorant@gmail.com>
2022-06-20 12:47:45 -0500

    agragar electoral de tamps, ags, hgo y df 12. También alianzas de df 12

--- data-raw/bdos_nac_auxiliares.R
+++ data-raw/bdos_nac_auxiliares.R
@@ -42,3 +42,36 @@ alianzas_pr_18 %>% count(coaliciones)
 
 alianzas_pr_18 %>% write_rds("inst/alianzas/nacional/pr_18.rda")
 
+## DF 12
+
+#### sacar alianzas del documento del ife de resultados por candidato
+
+alianza_pri_pven <- candidatos_df_12 %>% janitor::clean_names()  %>%
+  mutate(candidatura_comun = if_else(is.na(pri), TRUE,FALSE),
+         coaliciones = if_else(is.na(pri_pvem),"NA","pri_pvem")) %>%
+  select(id_estado,id_distrito,coaliciones, candidatura_comun) %>%
+  filter(coaliciones != "NA")
+
+alianza_prd_pt_mc <- candidatos_df_12 %>% janitor::clean_names()  %>%
+  mutate(candidatura_comun = !is.na(prd_pt_mc),
+         coaliciones = if_else(is.na(prd_pt_mc),"NA","prd_pt_mc")) %>%
+  select(id_estado,id_distrito,coaliciones, candidatura_comun) %>%
+  filter(coaliciones != "NA")
+
+alianza_pri_pven %>% full_join(alianza_prd_pt_mc) %>% write_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/alianzas/federales/df_12.csv")
+
+# subir alianzas df 12 al paquete
+
+alianzas_df_12 <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/Limpieza/alianzas/federales/df_12.csv")
+
+alianzas_df_12 <- alianzas_df_12 %>%
+  rename(estado = id_estado,
+         distritof_12 = id_distrito) %>%
+  mutate(eleccion = "df_12",
+         estado = formatC(estado,width = 2,flag = "0"),
+         distritof_12 = formatC(distritof_12,width = 2,flag = "0"))
+
+alianzas_df_12 %>% count(coaliciones)
+
+alianzas_df_12 %>% write_rds("inst/alianzas/nacional/pr_12.rda")
+

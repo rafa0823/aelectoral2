a6bcacee29245db7b82bfec0fc8d53bcc06e99af
EAMI91 <emiliomorones@gmail.com>
2022-12-27 12:20:33 -0600

    debug relativizar censo

--- R/analisis_censos.R
+++ R/analisis_censos.R
@@ -7,19 +7,24 @@
 #'
 #' @examples
 relativizar_censo <- function(bd){
-  bd <- diccionario_censo20 %>%
-    filter(!is.na(denominador),
-           denominador!="-")
-  res <- purrr::map_dfc(.x=unique(bd$denominador),
-                        ~{numerador <- bd %>%
-                          filter(denominador==.x) %>%
-                          pull(variable)
-                        denominador=glue::glue("cen_{.x}")
-                        bd <- bd %>%
-                          select(glue::glue("cen_{numerador}"), denominador) %>%
-                          mutate(across(everything(),
-                                        ~.x/!!rlang::sym(denominador))) %>%
-                          select(-denominador)})
+  dicc <- diccionario_censo20 %>%
+    dplyr::filter(!is.na(denominador),
+                  denominador!="-")
+  dicc <- dicc %>%
+    filter(glue::glue("cen_{variable}") %in% names(bd),
+           glue::glue("cen_{denominador}") %in% names(bd)
+    )
+  res <- purrr::map_dfc(.x=unique(dicc$denominador),
+                        ~{
+                          numerador <- dicc %>%
+                            dplyr::filter(denominador==.x) %>%
+                            pull(variable)
+                          denominador=glue::glue("cen_{.x}")
+                          bd <- bd %>%
+                            select(glue::glue("cen_{numerador}"), denominador) %>%
+                            mutate(across(everything(),
+                                          ~.x/!!rlang::sym(denominador))) %>%
+                            select(-denominador)})
 
   res <- bd %>%
     select(-names(res)) %>%

ae7090737f659bdda8462f1b5b8b4080ad68fbcc
Rafael Lopez <rafa0823.rl@gmail.com>
2023-08-24 09:36:39 -0600

    gb_18 gto

---
+++ dev/02_dev.R
@@ -0,0 +1,6 @@
+## En este archivo se utilizarán las funciones de usethis
+## para modificar la estructura del paquete
+
+#Guanajuato
+usethis::use_data_raw("bdos_gto_locales")
+usethis::use_data_raw("bdos_gto_auxiliares")

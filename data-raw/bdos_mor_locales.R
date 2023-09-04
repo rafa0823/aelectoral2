7f9ee4f06e0661ce13d98a3ed97362fd8804ee41
Rafael Lopez <rafa0823.rl@gmail.com>
2023-09-04 09:01:26 -0600

    dl_18

--- data-raw/bdos_mor_locales.R
+++ data-raw/bdos_mor_locales.R
@@ -112,5 +112,51 @@ aux |>
 glimpse(aux)
 pm_18 <- aux
 write_rds(pm_18, file = "inst/electoral/mor/pm_18.rda")
+# dl_18 -------------------------------------------------------------------
+path <- "~/Google Drive/Unidades compartidas/2_Recursos/Externas/Limpieza/Resultados definitivos/Local/2018/Distrito local/morelos"
+temp <- list.files(path, full.names = T) %>%
+  map_df(~{
+    read_excel(.x, skip = 1) |>
+      janitor::remove_empty(which = "rows") |>
+      janitor::clean_names()
+  })
 
+glimpse(temp)
+aux <- temp |>
+  rename_with(~gsub("votos_", "", .x), .cols = contains("votos_")) |>
+  rename_with(~gsub("coal_", "", .x), .cols = contains("coal_")) |>
+  rename_with(~gsub("config_", "", .x), .cols = contains("config_")) |>
+  rename_with(~gsub("cand_", "", .x), .cols = contains("cand_")) |>
+  rename(panal = na,
+         pes = es,
+         ph = humanista,
+         noreg = no_registrados,
+         nulos = num_nulos,
+         total = total_de_votos,
+         nominal = lista_nominal) |>
+  mutate(seccion = if_else(seccion == 1000, "0000", sprintf('%04s', seccion)),
+         casilla = if_else(seccion == 1000, "S1", casilla),
+         pt_morena_pes = sum(pt_morena_pes, cc_pt_morena_pes, na.rm = T),
+         pt_morena = sum(pt_morena, cc_pt_morena, na.rm = T),
+         morena_pes = sum(morena_pes, cc_morena_pes, na.rm = T),
+         pt_pes = sum(pt_pes, cc_pt_pes, na.rm = T)
+  ) |>
+  select(-contains("cc_")) |>
+  homologar_bd(estado = "17", nombre_estado = "MORELOS") |>
+  left_join(select(rel_18, clave_casilla, distritol_18, contains("municipio")), join_by(clave_casilla)) |>
+  select(estado, nombre_estado, distritol_18, contains("municipio"), seccion, clave_casilla, id_casilla:ext_contigua, casilla, everything())
 
+viejos <- colnames(aux)[grepl("ind_", colnames(aux))]
+nuevos <- paste0("ind", seq_along(viejos))
+
+aux <- aux |>
+  rename_at(all_of(viejos), ~nuevos) |>
+  rename_with(~paste0("ele_", .x, "_dl_18"), .cols = c(pan:ind7)) |>
+  mutate_all(~tidyr::replace_na(., 0))
+
+aux |>
+  count(nchar(clave_casilla))
+
+glimpse(aux)
+dl_18 <- aux
+write_rds(dl_18, file = "inst/electoral/mor/dl_18.rda")

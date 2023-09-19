9b3e066a8218ec5f2eb6a77a2285076e4ca828c0
Rafael Lopez <rafa0823.rl@gmail.com>
2023-09-19 13:54:05 -0600

    Primeros tests de las funciones

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -1,6 +1,8 @@
 ## En este archivo se utilizarán las funciones de usethis
 ## para modificar la estructura del paquete
 
+usethis::use_testthat(3)
+
 #Guanajuato
 usethis::use_data_raw("bdos_gto_locales")
 usethis::use_data_raw("bdos_gto_auxiliares")
@@ -15,3 +17,6 @@ usethis::use_data_raw("bdos_mor_locales")
 usethis::use_data_raw("bdos_mor_auxiliares")
 usethis::use_data_raw("bdos_ver_locales")
 usethis::use_data_raw("bdos_ver_auxiliares")
+usethis::use_test("estructura")
+usethis::use_test("analisis_explorar")
+usethis::use_test("ags")

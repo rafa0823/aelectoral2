023aab6b4e40947de9b97a1a94ee43337b75a85a
Rafael Lopez <rafa0823.rl@gmail.com>
2023-11-17 16:38:21 -0600

    Actualización de la paleta de colores (rsp, fxm, pes)

--- data-raw/paleta.R
+++ data-raw/paleta.R
@@ -1,7 +1,4 @@
 ## code to prepare `paleta` dataset goes here
-partidos <- c("morena", "pri", "pan", "prd", "mc", "panal", "pt", "total")
-colores <- c("#A6032F", "#038C33", "#0339A6", "#F2B705","#f27405","#03A6A6", "#D91136", "#FF8FAB")
-alcance <- rep("nacional", times = length(colores))
+paleta <- readr::read_csv("data-raw/paleta.csv")
 
-paleta <- tibble(partidos, colores, alcance)
 usethis::use_data(paleta, overwrite = TRUE)

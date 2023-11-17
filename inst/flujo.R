fd6a53b06a22af60fe4413eab9a5d4288ab5c9dc
EAMI91 <emiliomorones@gmail.com>
2023-11-17 14:18:23 -0600

    Clase Tablero funcional para múltiples niveles y elecciones

--- inst/flujo.R
+++ inst/flujo.R
@@ -14,16 +14,13 @@ c("dl_21", "pm_21") |>
     # cdmx_secc$voto_relativo(base = "bd_partido", partidos = paleta$partidos, eleccion = .x)
     # cdmx_secc$calcular_ganador(base = "bd_partido", partidos = paleta$partidos, eleccion = .x)
     cdmx_secc$obtener_degradado_ganador(base = "bd_partido",
-                                        colores_nombrados = set_names(paleta$colores, paleta$partidos),
                                         eleccion = .x)
   })
 
 shp <- ElectoralSHP$new(unidad = "secc_22", entidad = "cdmx")
 shp$agregar_shp("mun_22", entidad = "cdmx")
 shp$agregar_shp("df_22", entidad = "cdmx")
-shp$shp$secc_22_cdmx
-shp$shp$mun_22_cdmx
-shp$shp$df_22_cdmx
+
 cdmx_secc$colapsar_base("bd_partido", filtro = shp$shp$secc_22_cdmx |>
                           filter(distritol_22 == "09_18") |>
                           as_tibble() |>
@@ -35,8 +32,11 @@ cdmx_secc$fusionar_shp(shp = shp$shp$secc_22_cdmx,
                        base = "bd_partido")
 
 tablero <- Tablero$new(info_seccion = cdmx_secc)
-tablero$agregar_eleccion |> debug()
-tablero$agregar_eleccion(elecciones = c("df_21"),
+# tablero$agregar_eleccion |> debug()
+
+# IMPORTANTE!!!!! hacer relación de municipio inegi y municipio ine
+# tablero$agregar_eleccion |> debug()
+tablero$agregar_eleccion(elecciones = c("df_21", "dl_21", "pm_21"),
                          nivel = "municipio_22",
                          bd_relacion = shp$shp$secc_22_cdmx |>
                            as_tibble() |>
@@ -48,7 +48,7 @@ tablero$info$shp |>
   pluck("municipio_22")
 
 
-tablero$agregar_eleccion(elecciones = c("df_21"),
+tablero$agregar_eleccion(elecciones = c("df_21", "dl_21", "pm_21"),
                          nivel = "distritof_22",
                          bd_relacion = shp$shp$secc_22_cdmx |>
                            as_tibble() |>

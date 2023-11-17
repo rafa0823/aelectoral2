caee6ad0eee561ee8a1ee30cb045c10abcb584c7
EAMI91 <emiliomorones@gmail.com>
2023-11-17 13:39:06 -0600

    censo por distritof_22, distritol_22 y municipio

--- data-raw/censo.R
+++ data-raw/censo.R
@@ -13,3 +13,42 @@ censo <- censo |>
   select(-id, - distrito)
 
 readr::write_rds(censo, "inst/censo/seccion_2020.rda")
+
+
+# censo distritof y distritol 22 ------------------------------------------
+
+shp <- ElectoralSHP$new(unidad = "secc_22", entidad = diccionario$abreviatura[[1]])
+walk(diccionario$abreviatura[-1],~shp$agregar_shp(unidad = "secc_22", entidad = .x))
+
+relacion <- shp$shp |>
+  bind_rows() |>
+  as_tibble() |>
+  select(seccion, distritof_22, distritol_22 )
+
+censo_df22 <- censo |>
+  left_join(relacion) |>
+  filter(!is.na(distritof_22)) |>
+  summarise(across(where(is.numeric), ~sum(.x, na.rm = T)),
+                                       .by = c(entidad, distritof_22))
+
+censo_dl22 <- censo |>
+  left_join(relacion) |>
+  filter(!is.na(distritol_22)) |>
+  summarise(across(where(is.numeric), ~sum(.x, na.rm = T)),
+            .by = c(entidad, distritol_22))
+
+readr::write_rds(censo_dl22, "inst/censo/distritol_22_2020.rda")
+
+
+# censo por municipio -----------------------------------------------------
+
+csv <- "~/Google Drive/Unidades compartidas/Morant Consultores/Insumos/INEGI/Censo 2020/Datos geografico/ITER_NALCSV20.csv"
+censo_mun <- read_csv(csv) |>
+  janitor::clean_names()
+
+censo_mun <- censo_mun |>
+  mutate(municipio = paste(entidad, mun, sep = "_")) |>
+  filter(nom_loc == "Total del Municipio") |>
+  select(pobtot:vph_sintic)
+
+readr::write_rds(censo_mun, "inst/censo/municipio_2020.rda")

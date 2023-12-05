2344ba9d95e75cb182574c7b31751426a7dc4666
Rafael Lopez <rafa0823.rl@gmail.com>
2023-12-05 11:54:44 -0600

    Corrección gb_18

--- data-raw/bdos_mor_locales.R
+++ data-raw/bdos_mor_locales.R
@@ -8,7 +8,11 @@ homologar_bd <- function(bd, estado, nombre_estado){
   bd |>
     mutate(estado = !!estado,
            nombre_estado = nombre_estado,
-           casilla = if_else(casilla == "B", "B01",casilla),
+           casilla = case_when(casilla == "B"~ "B01",
+                               grepl("MEC", casilla) ~ gsub("MEC", "M", casilla),
+                               grepl("VA", casilla) ~ gsub("VA", "V", casilla),
+                               grepl("VPPP", casilla) ~ gsub("VPPP", "P", casilla),
+                               T ~casilla),
            id_casilla = case_when(nchar(casilla) >= 4 ~ stringr::str_extract_all(casilla,"(?<=E)[^C]*?(\\d+)(?=C)"),
                                   T ~ stringr::str_extract_all(casilla,"(?<=[a-zA-Z])(\\d+)")),
            tipo_casilla = substr(casilla, 1, 1),
@@ -18,95 +22,61 @@ homologar_bd <- function(bd, estado, nombre_estado){
     tidyr::unnest(cols = c(casilla:ext_contigua))
 }
 
-# relacion 18 --------------------------------------------------------------------
-path <- "~/Google Drive/Unidades compartidas/2_Recursos/Externas/Limpieza/Resultados definitivos/Local/2018/Municipio/nombres_mun_morelos.csv"
-mun <- read_csv(path, skip = 4) |>
-  janitor::clean_names() |>
-  distinct(municipio_18 = id_distrito_local_o_id_municipio, nombre_municipio_18 = toupper(distrito_local_o_municipio),
-           seccion)
-
-path <- "~/Google Drive/Unidades compartidas/2_Recursos/Externas/Limpieza/Resultados definitivos/Local/2018/Gobernador/morelos_normal_casilla.csv"
-rel_18 <- read_csv(path) |>
-  janitor::clean_names() |>
-  mutate(clave_casilla = gsub("'", "", clave_casilla)) |>
-  distinct(clave_casilla, estado = as.character(estado), nombre_estado,
-           distritol_18 = sprintf("%02d", distrito_local),
-           seccion = sprintf("%04d", seccion), nominal) |>
-  left_join(mun)
-
 # gb_18 -------------------------------------------------------------------
-path <- "~/Google Drive/Unidades compartidas/2_Recursos/Externas/Limpieza/Resultados definitivos/Local/2018/Gobernador/morelos"
-temp <- list.files(path, full.names = T) %>%
-  map_df(~{
-    read_excel(.x, skip = 1) |>
-      janitor::remove_empty(which = "rows") |>
-      janitor::clean_names()
-  })
-
-aux <- temp |>
-  rename_with(~gsub("votos_", "", .x), .cols = contains("votos_")) |>
-  rename_with(~gsub("coal_", "", .x), .cols = contains("coal_")) |>
-  rename_with(~gsub("config_", "", .x), .cols = contains("config_")) |>
+path <- "~/Google Drive/Unidades compartidas/3_Insumos/Externas/Limpieza/PEL/MOR/MOR_PEL_2018/GUBERNATURA_csv/2018_SEE_GOB_MOR_CAS.csv"
+aux <- read_csv(path) |>
+  janitor::clean_names() |>
+  rename_with(~gsub("num_votos_", "", .x), contains("num_votos_")) |>
+  rename_with(~gsub("cand_", "", .x), contains("cand_")) |>
   rename(panal = na,
-         pes = es,
          ph = humanista,
-         ind = cand_ind_fidel_demedicis,
-         noreg = no_registrados,
-         nulos = num_nulos,
-         total = total_de_votos,
-         nominal = lista_nominal) |>
-  mutate(seccion = if_else(seccion == 1000, "0000", sprintf('%04s', seccion)),
-         casilla = if_else(seccion == 1000, "S1", casilla)) |>
-  homologar_bd(estado = "17", nombre_estado = "MORELOS") |>
-  left_join(select(rel_18, clave_casilla, distritol_18, contains("municipio")), join_by(clave_casilla)) |>
-  select(estado, nombre_estado, distritol_18, contains("municipio"), seccion, clave_casilla, id_casilla:ext_contigua, casilla, everything()) %>%
-  rename_with(~paste0("ele_", .x, "_gb_18"), .cols = c(pan:nominal))
+         noreg = can_nreg,
+         total = total_votos,
+         nominal = lista_nominal,
+         distritol_18 = id_distrito_local,
+         nombre_distritol_18 = cabecera_distrital_local,
+         nombre_municipio_18 = municipio,
+         municipio_18 = id_municipio) |>
+  rename_with(~paste0("ele_", .x, "_gb_18"), .cols = c(pan:nominal)) |>
+  rename_with(~gsub("_es_", "_pes_", .x), contains("_es_")) |>
+  homologar_bd(estado = "17", "MORELOS") |>
+  mutate(distritol_18 = sprintf("17_%02s", distritol_18),
+         municipio_18 = sprintf("17_%03s", municipio_18),
+         seccion = sprintf("17_%04s", seccion)) |>
+  select(estado, nombre_estado, distritol_18:clave_casilla, -c(circunscripcion, estatus_acta:ruta_acta)) |>
+  relocate(clave_casilla, .after = seccion)
 
 glimpse(aux)
 gb_18 <- aux
 write_rds(gb_18, file = "inst/electoral/mor/gb_18.rda")
 # pm_18 -------------------------------------------------------------------
-path <- "~/Google Drive/Unidades compartidas/2_Recursos/Externas/Limpieza/Resultados definitivos/Local/2018/Municipio/morelos"
-temp <- list.files(path, full.names = T) %>%
-  map_df(~{
-    read_excel(.x, skip = 1) |>
-      janitor::remove_empty(which = "rows") |>
-      janitor::clean_names()
-  })
 
-
-aux <-temp |>
-  rename_with(~gsub("votos_", "", .x), .cols = contains("votos_")) |>
-  rename_with(~gsub("coal_", "", .x), .cols = contains("coal_")) |>
-  rename_with(~gsub("config_", "", .x), .cols = contains("config_")) |>
-  rename_with(~gsub("cand_", "", .x), .cols = contains("cand_")) |>
+path <- "~/Google Drive/Unidades compartidas/3_Insumos/Externas/Limpieza/PEL/MOR/MOR_PEL_2018/AYUNTAMIENTOS_csv/2018_SEE_AYUN_MOR_CAS.csv"
+aux <- read_csv(path) |>
+  janitor::clean_names() |> glimpse()
+  rename_with(~gsub("num_votos_", "", .x), contains("num_votos_")) |>
+  rename_with(~gsub("cand_", "", .x), contains("cand_")) |>
   rename(panal = na,
          ph = humanista,
-         noreg = no_registrados,
-         nulos = num_nulos,
-         total = total_de_votos,
-         nominal = lista_nominal) |>
-  mutate(seccion = if_else(seccion == 1000, "0000", sprintf('%04s', seccion)),
-         casilla = if_else(seccion == 1000, "S1", casilla),
-         pt_morena_pes = sum(pt_morena_pes, cc_pt_morena_pes, na.rm = T),
-         pt_morena = sum(pt_morena, cc_pt_morena, na.rm = T),
-         morena_pes = sum(morena_pes, cc_morena_pes, na.rm = T)
-  ) |>
-  select(-contains("cc_")) |>
-  homologar_bd(estado = "17", nombre_estado = "MORELOS") |>
-  left_join(select(rel_18, clave_casilla, distritol_18, contains("municipio")), join_by(clave_casilla)) |>
-  select(estado, nombre_estado, distritol_18, contains("municipio"), seccion, clave_casilla, id_casilla:ext_contigua, casilla, everything())
+         noreg = can_nreg,
+         total = total_votos,
+         nominal = lista_nominal,
+         distritol_18 = id_distrito_local,
+         nombre_distritol_18 = cabecera_distrital_local,
+         nombre_municipio_18 = municipio,
+         municipio_18 = id_municipio) |>
+  rename_with(~paste0("ele_", .x, "_gb_18"), .cols = c(pan:ind1)) |>
+  rename_with(~gsub("_es_", "_pes_", .x), contains("_es_")) |>
+  homologar_bd(estado = "17", "MORELOS") |>
+  mutate(distritol_18 = sprintf("17_%02s", distritol_18),
+         municipio_18 = sprintf("17_%03s", municipio_18),
+         seccion = sprintf("17_%04s", seccion)) |>
+  select(estado, nombre_estado, distritol_18:clave_casilla)
 
-viejos <- colnames(aux)[grepl("ind_", colnames(aux))]
-nuevos <- paste0("ind", seq_along(viejos))
+glimpse(aux)
+gb_18 <- aux
+write_rds(gb_18, file = "inst/electoral/mor/gb_18.rda")
 
-aux <- aux |>
-  rename_at(all_of(viejos), ~nuevos) |>
-  rename_with(~paste0("ele_", .x, "_pm_18"), .cols = c(pan:ind29)) |>
-  mutate_all(~tidyr::replace_na(., 0))
-
-aux |>
-  count(nchar(clave_casilla))
 
 glimpse(aux)
 pm_18 <- aux

59e81bbd450b2f21722c5189ecca829b08a9db67
Rafael Lopez <rafa0823.rl@gmail.com>
2024-03-05 16:16:48 -0600

    Inclusión de colores 2006 a paleta

--- data-raw/paleta.R
+++ data-raw/paleta.R
@@ -1,4 +1,10 @@
 ## code to prepare `paleta` dataset goes here
-paleta <- readr::read_csv("data-raw/paleta.csv")
+bd_2006 <- tibble(partidos = c("pbt", "apm", "asdc"),
+                  colores = c("#fcbf49", "#588157", "#ffb3c6"),
+                  alcance = rep("nacional", 3))
+
+
+paleta <- readr::read_csv("data-raw/paleta.csv") |>
+  bind_rows(bd_2006)
 
 usethis::use_data(paleta, overwrite = TRUE)

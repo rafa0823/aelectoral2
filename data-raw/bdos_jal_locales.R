f4405b6f1a822c5cb7172fa480b58e290196c744
Rafael Lopez <rafa0823.rl@gmail.com>
2024-07-01 10:43:43 -0600

    Elecciones 2015 Jalisco

--- data-raw/bdos_jal_locales.R
+++ data-raw/bdos_jal_locales.R
@@ -1,14 +1,16 @@
 ## code to prepare `bdos_jal_locales` dataset goes here
 library(tidyverse)
-
-
 # Funciones ---------------------------------------------------------------
 
 homologar_bd <- function(bd, estado, nombre_estado){
   bd |>
     mutate(estado = !!estado,
            nombre_estado = nombre_estado,
-           casilla = if_else(casilla == "B", "B01",casilla),
+           casilla = case_when(casilla == "B"~ "B01",
+                               grepl("MEC", casilla) ~ gsub("MEC", "M", casilla),
+                               grepl("VA", casilla) ~ gsub("VA", "V", casilla),
+                               grepl("VPPP", casilla) ~ gsub("VPPP", "P", casilla),
+                               T ~casilla),
            id_casilla = case_when(nchar(casilla) >= 4 ~ stringr::str_extract_all(casilla,"(?<=E)[^C]*?(\\d+)(?=C)"),
                                   T ~ stringr::str_extract_all(casilla,"(?<=[a-zA-Z])(\\d+)")),
            tipo_casilla = substr(casilla, 1, 1),
@@ -211,3 +213,76 @@ pm_21 |>
   count(nchar(clave_casilla))
 
 write_rds(pm_21, "inst/electoral/jal/pm_21.rda")
+# Elecciones 2015 ---------------------------------------------------------
+# pm 15 -------------------------------------------------------------------
+path_15 <- "~/Google Drive/Unidades compartidas/Morant Consultores/Insumos/INE/computos/JALISCO/JAL_PEL_2015/AYUNTAMIENTOS_csv/2015_SEE_AYUN_JAL_CAS.csv"
+estado <- "jal"
+nombre_estado <- "JALISCO"
+id_estado <- "14"
+eleccion <- "pm_15"
+
+aux <- readr::read_csv(path_15) |>
+  janitor::clean_names() |>
+  rename_with(~gsub("num_votos_", "", .x), contains("num_votos_")) |>
+  rename_with(~gsub("cand_|can_", "", .x), contains(c("cand_", "can_"))) |>
+  rename(
+    panal = nva_alianza,
+    # pes = es,
+    noreg = nreg,
+    total = total_votos,
+    nominal = lista_nominal,
+    !!glue::glue("distritol_{readr::parse_number(eleccion)}") := id_distrito,
+    !!glue::glue("nombre_distritol_{readr::parse_number(eleccion)}") := cabecera_distrital,
+    !!glue::glue("nombre_municipio_{readr::parse_number(eleccion)}") := municipio,
+    !!glue::glue("municipio_{readr::parse_number(eleccion)}") := id_municipio
+  ) |>
+  rename_with(~paste("ele", .x, eleccion, sep = "_"), .cols = c(pan:nominal)) |>
+  # rename_with(~gsub("_es_", "_pes_", .x), contains("_es_")) |>
+  rename_with(~gsub("_nva_alianza_", "_panal_", .x), contains("_nva_alianza_")) |>
+  homologar_bd(estado = id_estado, nombre_estado = nombre_estado) |>
+  mutate(distritol_15 = sprintf("%02s", distritol_15),
+         municipio_15 = sprintf("%03s", municipio_15),
+         seccion = sprintf("%04s", seccion)) |>
+  select(estado, nombre_estado, distritol_15:clave_casilla, -c(circunscripcion, estatus_acta:ruta_acta)) |>
+  relocate(clave_casilla, .after = seccion)
+
+aux |>
+  count(nchar(clave_casilla))
+
+glimpse(aux)
+write_rds(aux, file = glue::glue("inst/electoral/{estado}/{eleccion}.rda"))
+
+# pm 15 -------------------------------------------------------------------
+path_15 <- "~/Google Drive/Unidades compartidas/Morant Consultores/Insumos/INE/computos/JALISCO/JAL_PEL_2015/DIPUTACIONES_LOC_MR_csv/2015_SEE_DIP_LOC_MR_JAL_CAS.csv"
+eleccion <- "dl_15"
+
+aux <- readr::read_csv(path_15) |>
+  janitor::clean_names() |>
+  rename_with(~gsub("num_votos_", "", .x), contains("num_votos_")) |>
+  rename_with(~gsub("cand_|can_", "", .x), contains(c("cand_", "can_"))) |>
+  rename(
+    panal = nva_alianza,
+    # pes = es,
+    noreg = nreg,
+    total = total_votos,
+    nominal = lista_nominal,
+    !!glue::glue("distritol_{readr::parse_number(eleccion)}") := id_distrito,
+    !!glue::glue("nombre_distritol_{readr::parse_number(eleccion)}") := cabecera_distrital,
+    !!glue::glue("nombre_municipio_{readr::parse_number(eleccion)}") := municipio,
+    !!glue::glue("municipio_{readr::parse_number(eleccion)}") := id_municipio
+  ) |>
+  rename_with(~paste("ele", .x, eleccion, sep = "_"), .cols = c(pan:nominal)) |>
+  rename_with(~gsub("_es_", "_pes_", .x), contains("_es_")) |>
+  rename_with(~gsub("_nva_alianza_", "_panal_", .x), contains("_nva_alianza_")) |>
+  homologar_bd(estado = id_estado, nombre_estado = nombre_estado) |>
+  mutate(distritol_15 = sprintf("%02s", distritol_15),
+         municipio_15 = sprintf("%03s", municipio_15),
+         seccion = sprintf("%04s", seccion)) |>
+  select(estado, nombre_estado, distritol_15:clave_casilla, -c(circunscripcion, estatus_acta:ruta_acta)) |>
+  relocate(clave_casilla, .after = seccion)
+
+aux |>
+  count(nchar(clave_casilla))
+
+glimpse(aux)
+write_rds(aux, file = glue::glue("inst/electoral/{estado}/{eleccion}.rda"))
